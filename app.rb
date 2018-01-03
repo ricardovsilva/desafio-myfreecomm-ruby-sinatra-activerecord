a77520a5ff34e7e1a2fcc924546c3b2f9a00a0bc
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2018-01-03 14:15:47 -0200

    Cria o formulário para submissão do arquivo

--- app.rb
+++ app.rb
@@ -3,3 +3,14 @@ require 'sinatra/activerecord'
 
 set :database, {adapter: "sqlite3", database: "db/solution.sqlite3"}
 
+get '/' do
+  erb :index
+end
+
+get '/purchases' do
+  
+end
+
+post '/purchases' do
+
+end
